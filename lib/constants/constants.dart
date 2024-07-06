993cf723bb26750d3685599d8d3db0e0743792f9
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-07-06 15:28:49 +0530

    Initial commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class Constants {
+  static const String apiKey = '7a9c44fe0cff76ce3347636b8015630b';
+  static const String apiKey2 = '19d2fd7aab958bfe7f61000e1f5151cc';
+}
